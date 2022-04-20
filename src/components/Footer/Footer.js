577b83f7047e626ac5eb75e04e4d166c488f9bfe
Kethan Vemuri <kethan.vemuri@manpowergroup.nl>
2022-04-21 00:47:04 +0530

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,38 @@
 import React from 'react'
-import { ImFacebook, ImWhatsapp, ImGithub } from 'react-icons/im'
-
+import { ImFacebook, ImWhatsapp, ImGithub, ImPhone } from 'react-icons/im'
+import { SiGmail } from 'react-icons/si'
 const Footer = () => {
   return (
-      <div className='bg-gray-800 py-3 px-5 text-center text-white'>
-          <ul className='w-half m-auto my-3 flex items-center justify-center'>
-            <li className='mx-3'><a href='#'><ImFacebook className='text-3xl hover:text-sky-500'/></a></li>
-            <li className='mx-3'><a href='#'><ImWhatsapp className='text-3xl hover:text-green-300'/></a></li>
-            <li className='mx-3'><a href='#'><ImGithub className='text-3xl hover:text-cyan-300'/></a></li>
-            <li className='mx-3'><a href='#'><ImGithub className='text-3xl hover:text-cyan-300'/></a></li>
-          </ul>
-          Copyright &copy; 2020 | <a href="#" className='hover:text-sky-500'>Portfolio Kethan</a>
-      </div>
+    <div className='bg-gray-800 py-3 px-5 text-center text-white'>
+      <ul className='w-half m-auto my-3 flex items-center justify-center'>
+        <li className='mx-3'>
+          <a target="_blank" href='https://www.facebook.com/kethan.sai.735/'>
+            <ImFacebook className='text-3xl hover:text-sky-500 transition ease-in-out duration-200 hover:-translate-y-1 hover:scale-110' />
+          </a>
+        </li>
+        <li className='mx-3'>
+          <a target="_blank" href='https://wa.me/917989216155?text=Hi Kethan Vemuri'>
+            <ImWhatsapp className='text-3xl hover:text-green-500 transition ease-in-out duration-200 hover:-translate-y-1 hover:scale-110' />
+          </a>
+        </li>
+        <li className='mx-3'>
+          <a target="_blank" href='https://github.com/kethansai'>
+            <ImGithub className='text-3xl transition ease-in-out duration-200 hover:-translate-y-1 hover:scale-110' />
+          </a>
+        </li>
+        <li className='mx-3'>
+          <a target="_blank" href='mailto:kethansai77@gmail.com'>
+            <SiGmail className='text-3xl hover:text-red-500 transition ease-in-out duration-200 hover:-translate-y-1 hover:scale-110' />
+          </a>
+        </li>
+        <li className='mx-3'>
+          <a target="_blank" href='tel:+917989216155'>
+            <ImPhone className='text-3xl hover:text-cyan-500 transition ease-in-out duration-200 hover:-translate-y-1 hover:scale-110' />
+          </a>
+        </li>
+      </ul>
+      Copyright &copy; 2020 | <a href="https://kethansai.github.io/portfolio/" className='hover:text-sky-500'>Portfolio Kethan Vemuri</a>
+    </div>
   )
 }
 
--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -8,69 +8,67 @@ import { ImCross } from 'react-icons/im'
 
 const Navbar = () => {
   let [flag, setflag] = useState(false);
+  const toggleFlag = () => {
+    setflag(!flag)
+  }
+  const mobileMenu = [
+    {  href: "#", text: "Home" },
+    {  href: "#", text: "Experience" },
+    {  href: "#", text: "Education" },
+    {  href: "#", text: "Skills" },
+    {  href: "#", text: "Projects" },
+    {  href: "#", text: "Internships" },
+    {  href: "#", text: "Contact Me" },
+    { href: "mailto:kethansai77@gmail.com", text: "Hire Me" },
+  ];
+  const desktopMenu = [
+    { href: "#", text: "Home", icon: <FaHome className='text-xl mt-2.5 mx-1' />},
+    { href: "#", text: "Experience", icon: <FaBriefcase className='text-xl mt-2.5 mx-1' />},
+    { href: "#", text: "Education", icon: <FaUserGraduate className='text-xl mt-2.5 mx-1' />},
+    { href: "#", text: "Skills", icon: <AiFillSafetyCertificate className='text-xl mt-2.5 mx-1' /> },
+    { href: "#", text: "Projects", icon: <RiFileListFill className='text-xl mt-2.5 mx-1' />},
+    { href: "#", text: "Internships", icon: <HiOutlineOfficeBuilding className='text-xl mt-2.5 mx-1' />},
+    { href: "#", text: "Contact Me", icon: <RiMailAddFill className='text-xl mt-2.5 mx-1' />},
+    { href: "mailto:kethansai77@gmail.com", text: "Hire Me", icon: <FaCommentDollar className='text-xl mt-2.5 mx-1' />},
+  ];
   return (
     <div>
+
+      {/* Desktop Menu */}
+
       <div className='flex relative items-center justify-between uppercase card shadow-xl bg-gray-900 text-white py-4 md:py-3 px-12 md:px-12 leading-10'>
-        <div className='Logo item cursor-pointer  underline-offset-8 transition ease-in-out duration-200 font-bold 
+        <div className='Logo item text-4xl md:text-xl underline-offset-8 transition ease-in-out duration-200 font-bold 
               hover:text-cyan-300 hover:underline hover:-translate-y-1 hover:scale-110'>
             KSR
         </div>
         <ul className='hidden md:flex item-center justify-around'>
-            <li className='flex item mx-3 cursor-pointer underline-offset-8 transition ease-in-out duration-200
+          {
+            desktopMenu.map(item =>
+              <li className='flex item mx-3 underline-offset-8 transition ease-in-out duration-200
                       hover:font-bold hover:text-cyan-300 hover:underline hover:-translate-y-1 hover:scale-110'>
-                <FaHome className='text-xl mt-2.5 mx-1'/>
-                <a href="#">Home</a>
-            </li>
-            <li className='flex item mx-3 cursor-pointer underline-offset-8 transition ease-in-out duration-200
-                      hover:font-bold hover:text-cyan-300 hover:underline hover:-translate-y-1 hover:scale-110'>
-              <FaBriefcase className='text-xl mt-2.5 mx-1'/>  
-              <a href="#">Experience</a>
-            </li>
-            <li className='flex item mx-3 cursor-pointer underline-offset-8 transition ease-in-out duration-200
-                      hover:font-bold hover:text-cyan-300 hover:underline hover:-translate-y-1 hover:scale-110'>
-                <FaUserGraduate className='text-xl mt-2.5 mx-1' />
-                <a href="#"> Education</a>
-            </li>
-            <li className='flex item mx-3 cursor-pointer underline-offset-8 transition ease-in-out duration-200
-                      hover:font-bold hover:text-cyan-300 hover:underline hover:-translate-y-1 hover:scale-110'>
-              <AiFillSafetyCertificate className='text-xl mt-2.5 mx-1' />  
-              <a href="#">Skills</a>
-            </li>
-            <li className='flex item mx-3 cursor-pointer underline-offset-8 transition ease-in-out duration-200
-                      hover:font-bold hover:text-cyan-300 hover:underline hover:-translate-y-1 hover:scale-110'>
-              <RiFileListFill className='text-xl mt-2.5 mx-1'/>
-                <a href="#">Projects</a>
-            </li>
-            <li className='flex item mx-3 cursor-pointer underline-offset-8 transition ease-in-out duration-200
-                      hover:font-bold hover:text-cyan-300 hover:underline hover:-translate-y-1 hover:scale-110'>
-              <HiOutlineOfficeBuilding className='text-xl mt-2.5 mx-1' />
-              <a href="#">Internships</a>
-            </li>
-            <li className='flex item mx-3 cursor-pointer underline-offset-8 transition ease-in-out duration-200
-                      hover:font-bold hover:text-cyan-300 hover:underline hover:-translate-y-1 hover:scale-110'>
-              <RiMailAddFill className='text-xl mt-2.5 mx-1' />
-              <a href="#">Contact Me</a>
-            </li>
-            <li className='flex item mx-3 cursor-pointer underline-offset-8 transition ease-in-out duration-200
-                      hover:font-bold hover:text-cyan-300 hover:underline hover:-translate-y-1 hover:scale-110'>
-              <FaCommentDollar className='text-xl mt-2.5 mx-1' />
-              <a href="#">Hire Me</a>
-            </li>
+                {item.icon}
+                <a href={item.href}>{ item.text }</a>
+              </li>
+            )
+          }
         </ul>
         <div className='md:hidden text-2xl'>
-          <HiOutlineMenu className={flag === false ? 'block text-4xl' : 'hidden'} onClick={() => setflag(!flag)} />
-          <ImCross className={flag === true ? 'block text-4xl' : 'hidden'} onClick={() => flag = setflag(!flag)} />
+          <HiOutlineMenu className={flag === false ? 'block text-4xl' : 'hidden'} onClick={toggleFlag} />
+          <ImCross className={flag === true ? 'block text-4xl' : 'hidden'} onClick={toggleFlag} />
         </div>
       </div>
+      
+      {/* Mobile Menu */}
+
       <div className={flag === true ? 'block md:hidden' : 'hidden md:hidden'}>
-        <ul className='absolute flex-col items-center self-end py-8 space-y-6 font-bold bg-gray-900 text-white sm:w-auto
-        sm:self-center left-0 text-center right-0 drop-shadow-md'>
-          <li className='item cursor-pointer underline-offset-4 font-bold text-cyan-300 underline transition ease-in-out duration-200 hover:tracking-wide'>
-            <a href="#">HOME</a></li>
-          <li><a href="#">HOME</a></li>
-          <li><a href="#">HOME</a></li>
-          <li><a href="#">HOME</a></li>
-          <li><a href="#">HOME</a></li>
+        <ul className='absolute flex-col uppercase items-center self-end py-8 space-y-6 font-bold bg-gray-900 text-white sm:w-auto
+        sm:self-center text-3xl left-0 text-center right-0 drop-shadow-md'>
+          {
+            mobileMenu.map(item =>
+              <li className='item underline-offset-4 font-bold text-cyan-300 hover:underline transition ease-in-out duration-200 hover:tracking-wide'>
+                <a href={item.href} onClick={toggleFlag}>{item.text}</a></li>
+            )
+          }
         </ul>
       </div>
     </div>
